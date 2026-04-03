93067f4887f207462881eea3c1a95d0c4be6e929
Akshay301A <akshayraj20031430@gmail.com>
2026-04-03 15:55:25 +0530

    Use landing navbar/footer on public auth layout

--- CliniLocker/src/components/PublicLayout.tsx
+++ CliniLocker/src/components/PublicLayout.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react";
-import { Navbar } from "./Navbar";
-import { Footer } from "./Footer";
+import Navbar from "@/components/landing/Navbar";
+import Footer from "@/components/landing/Footer";
 
 export function PublicLayout({ children }: { children: ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 pt-24">{children}</main>
       <Footer />
     </div>
   );
